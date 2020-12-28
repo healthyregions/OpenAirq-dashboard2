5eb5b236c56292fa7a67bc796c2a23ffd5b108fe
Camen <camenpihor@gmail.com>
2020-12-28 15:12:27 -0800

    Add tests for logger

--- covid-weekly/tests/unit/test_logger.py
+++ covid-weekly/tests/unit/test_logger.py
@@ -1,5 +1,18 @@
+import io
+import re
+
 from covid_weekly import logger
 
 
 def test_create():
-    assert False
+    stream = io.StringIO()
+    _logger = logger.create(name="tests.test_logger", stream=stream)
+    _logger.info("this is a test message...")
+    actual = stream.getvalue()
+    assert bool(
+        re.match(
+            r"[A-Z]{4} \([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\) --"
+            r" .+ -- .+",
+            actual,
+        )
+    )
